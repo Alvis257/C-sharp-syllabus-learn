d986e9ad5f4713a88d2f692ed88026a9165b5a88
Alvis257 <https://github.com/Alvis257/JaunsRepos.git>
2021-11-24 03:41:08 +0200

    Excersise_8 complete

---
+++ csharp-basics/exercises/TypesAndVariables/Excersise_8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Excersise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Minutes,Days, Years, MinutesInAYear,RemeiningMinutes, MinutesInADay;
+            MinutesInAYear = 525600;
+            MinutesInADay = 1440;
+
+            Console.Write("Enter minutes: ");
+            Minutes = Convert.ToInt32(Console.ReadLine());
+            Years = Minutes / MinutesInAYear;
+            RemeiningMinutes = Minutes % MinutesInAYear;
+            Days = RemeiningMinutes / MinutesInADay;
+
+            Console.Write("This "+Minutes+" minutes are "+Years+" Years and "+Days+" Days");
+            Console.ReadKey();
+        }
+    }
+}
