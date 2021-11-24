36165c0cb32870981d4963c3a3a31f4d5a7f08bf
Alvis257 <https://github.com/Alvis257/JaunsRepos.git>
2021-11-24 03:03:25 +0200

    Excersise_7 complete

---
+++ csharp-basics/exercises/TypesAndVariables/Excersise_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Excersise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input;
+            int Count=0;
+            bool IsUperr;
+            IsUperr = true;
+
+            Console.Write("Enter a String: ");
+            Input=Console.ReadLine();
+
+            for(int i=0; i<Input.Length; i++)
+            {
+                if (IsUperr == char.IsUpper(Input[i])) 
+                {
+                    Count++;
+                }
+            }
+            Console.WriteLine("There are " + Count + " Uppercases in this string");
+            Console.ReadKey();
+        }
+    }
+}
