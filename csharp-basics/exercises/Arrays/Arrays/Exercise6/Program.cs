d27eeeaec29bda2ead322bdcda99eaffc184c97c
Alvis257 <https://github.com/Alvis257/JaunsRepos.git>
2021-11-30 20:43:32 +0200

    Exercise5

--- csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace Exercise6
 {
@@ -10,6 +7,7 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
