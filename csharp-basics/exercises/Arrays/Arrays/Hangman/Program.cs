98b77ca5a9457b960cf168c1918667409bb5dff8
Alvis257 <https://github.com/Alvis257/JaunsRepos.git>
2021-12-02 21:13:59 +0200

    Complete

---
+++ csharp-basics/exercises/Arrays/Arrays/Hangman/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            string[] words = {"happy", "sad", "galaxy", "new world"};
+            var misses = string.Empty;
+            var count = 0;
+            bool win = false;
+            Random rnd = new Random();
+            var index = rnd.Next(words.Length);
+            var wordToGuess = new char[words[index].Length];
+
+            foreach (var letter in words[index])
+            {
+                wordToGuess[count] = letter;
+                count++;
+            }
+
+            var hidenWord = new string[wordToGuess.Length];
+
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                if (wordToGuess[i] == ' ')
+                {
+                    hidenWord[i] = " ";
+                }
+                else hidenWord[i] = "_";
+            }
+
+            var tryes = 0;
+            Console.WriteLine($"Word: {string.Join(" ", hidenWord)}");
+            while (tryes < hidenWord.Length * 2)
+            {
+                count = 0;
+                index = 0;
+                Console.WriteLine($"Misses: {misses}");
+                Console.Write("Guess: ");
+                var guess = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                foreach (var letter in wordToGuess)
+                {
+                    if (guess == letter)
+                    {
+                        count = 1;
+                        hidenWord[index] = Convert.ToString(guess);
+                    }
+
+                    index++;
+                }
+
+                if (count != 1)
+                {
+                    misses += Convert.ToString(guess);
+                }
+
+                if (hidenWord.Contains("_") == false)
+                {
+                    tryes = hidenWord.Length * 2;
+                    win = true;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                Console.WriteLine();
+                Console.WriteLine($"Word: {string.Join(" ", hidenWord)}");
+                tryes++;
+            }
+
+            Console.WriteLine();
+
+            if (win == true)
+            {
+                Console.WriteLine($"You have guessed the word {string.Join(" ",wordToGuess)}");
+                reset();
+            }
+            else
+            {
+                Console.WriteLine($"You haven't guessed the word is  {string.Join(" ",wordToGuess)}");
+                reset();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void reset()
+        {
+            Console.WriteLine("Do you want to play agein y/n");
+            char input = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (input)
+            {
+                case 'y':
+                    Start();
+                    break;
+                case 'n':
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    reset();
+                    break;
+            }
+        }
+    }
+}
--- csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
@@ -14,23 +14,204 @@ namespace TicTacToe
 
         private static void InitBoard()
         {
-            // fills up the board with blanks
             for (var r = 0; r < 3; r++)
             {
                 for (var c = 0; c < 3; c++)
-                    board[r, c] = ' ';
+                    board[r, c] = '-';
             }
-            
         }
 
         private static void DisplayBoard()
         {
+            char player = 'X';
+            char winer, check;
+            int moveRow, moveColum, restart = 1;
+            int countX = 0, countO = 0;
+            bool gameEnd = false;
+            ConsoleKeyInfo input;
+
+            Console.WriteLine("    --+-+--");
             Console.WriteLine("  0  " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
             Console.WriteLine("    --+-+--");
             Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
             Console.WriteLine("    --+-+--");
             Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
             Console.WriteLine("    --+-+--");
+            while (restart == 1)
+            {
+                countX = 0;
+                countO = 0;
+                if (countX > countO)
+                {
+                    player = 'O';
+                }
+                else if (countX <= countO)
+                {
+                    player = 'X';
+                }
+
+                while (gameEnd != true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Player: {player}");
+                    Console.WriteLine("Enter youre move colum");
+                    input = Console.ReadKey();
+                    moveColum = int.Parse(input.KeyChar.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine("Enter youre move row");
+                    input = Console.ReadKey();
+                    moveRow = int.Parse(input.KeyChar.ToString());
+                    Console.WriteLine();
+
+                    if (board[moveColum, moveRow] == 'X' || board[moveColum, moveRow] == 'O')
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("This place is taken");
+                        continue;
+                    }
+                    else board[moveColum, moveRow] = player;
+
+
+                    Console.WriteLine("    --+-+--");
+                    Console.WriteLine("  0  " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
+                    Console.WriteLine("    --+-+--");
+                    Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
+                    Console.WriteLine("    --+-+--");
+                    Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
+                    Console.WriteLine("    --+-+--");
+
+                    countX = 0;
+                    countO = 0;
+                    foreach (var XO in board)
+                    {
+                        if (XO == 'X')
+                        {
+                            countX += 1;
+                        }
+                        else if (XO == 'O')
+                        {
+                            countO += 1;
+                        }
+                    }
+
+                    if (countX > countO)
+                    {
+                        player = 'O';
+                    }
+                    else if (countX <= countO)
+                    {
+                        player = 'X';
+                    }
+
+                    winer = getWinner();
+                    if (winer == 'O' || winer == 'X')
+                    {
+                        Console.WriteLine($"winer is {winer}");
+                        gameEnd = true;
+                    }
+                    else if (countO + countX == 9)
+                    {
+                        if (winer == '-')
+                        {
+                            Console.WriteLine("Its a Draw");
+                            gameEnd = true;
+                        }
+                    }
+                }
+                Console.WriteLine("Do you want to play agein y/n");
+                input = Console.ReadKey();
+                check = input.KeyChar;
+
+                if (check == 'y')
+                {
+                    restart = 1;
+                    gameEnd = false;
+                    InitBoard();
+                }
+                else restart = 0;
+            }
+            Console.ReadKey();
+        }
+
+        private static char getWinner()
+        {
+            if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2])
+            {
+                if (board[0, 0] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[0, 0];
+            }
+            else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
+            {
+                if (board[1, 0] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[1, 0];
+            }
+            else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
+            {
+                if (board[2, 0] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[2, 0];
+            }
+            else if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
+            {
+                if (board[0, 0] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[0, 0];
+            }
+            else if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
+            {
+                if (board[0, 1] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[0, 1];
+            }
+            else if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2])
+            {
+                if (board[0, 2] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[0, 2];
+            }
+            else if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                if (board[0, 0] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[0, 0];
+            }
+            else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                if (board[0, 2] == '-')
+                {
+                    return '-';
+                }
+                else
+                    return board[0, 2];
+            }
+            else
+            {
+                return '-';
+            }
         }
     }
 }
+
